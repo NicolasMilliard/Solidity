19a9700a84c5289301673a3516ecae47418f8bac
NicolasMilliard <contact@nicolasmilliard.fr>
2023-09-03 15:56:07 +0200

    Init commit

---
+++ contracts/StockPriceOracle.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title StockPriceOracle
+ * @author Nicolas Milliard
+ * @notice Allow an external source to update the price of a stock
+ */
+contract StockPriceOracle is Ownable {
+    string symbol;
+    /**
+     @dev stockPrice is stored as an integer with its decimals.
+     */
+    uint256 stockPrice;
+    uint256 nbDecimals;
+    uint256 lastUpdatedTimestamp;
+
+    event StockPriceUpdated(uint256 stockPrice, uint256 lastUpdatedTimestamp);
+
+    constructor(string memory _symbol, uint256 _nbDecimals) {
+        symbol = _symbol;
+        nbDecimals = _nbDecimals;
+    }
+
+    /**
+     @notice Set the updated stock price
+     @param _newPrice has to be sent with the right number of decimals even if their values are 0
+     @param _lastUpdatedTimestamp is optionnal but hihgly recommanded
+     */
+    function updatedPrice(
+        uint256 _newPrice,
+        uint256 _lastUpdatedTimestamp
+    ) external onlyOwner {
+        stockPrice = _newPrice;
+
+        /**
+         @dev Check if _lastUpdatedTimestamp has a specific value
+         */
+        _lastUpdatedTimestamp == 0
+            ? lastUpdatedTimestamp = block.timestamp
+            : lastUpdatedTimestamp = _lastUpdatedTimestamp;
+
+        emit StockPriceUpdated(stockPrice, lastUpdatedTimestamp);
+    }
+}
